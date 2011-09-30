d405a748fed2a17f0b8c9de8af9e613221a9c466
Gilliard Cordeiro <gscordeiro@gmail.com>
2011-09-30 13:25:02 -0300

    mudei o nome das variáveis

--- grails-app/taglib/jugmotors/CustomAjaxTagLib.groovy
+++ grails-app/taglib/jugmotors/CustomAjaxTagLib.groovy
@@ -3,17 +3,17 @@ package jugmotors
 class CustomAjaxTagLib {
 	def ajaxFunction = { attrs, body ->
 		
-		def lista = attrs.params.collect{ key, value -> "\'$key=\' + $value" }
-		def lista2 = attrs.ids.collect{ key, value -> "\'$key=\' + \$(\'#${value.replaceAll('\\.', '\\\\\\\\.')}\').val()" }
-		def lista3 = attrs.names.collect{ key, value -> "\'$key=\' + \$(\'[name=${value.replaceAll('\\.', '\\\\\\\\.')}]\').val()" }
+		def params = attrs.params.collect{ key, value -> "\'$key=\' + $value" }
+		def ids = attrs.ids.collect{ key, value -> "\'$key=\' + \$(\'#${value.replaceAll('\\.', '\\\\\\\\.')}\').val()" }
+		def names = attrs.names.collect{ key, value -> "\'$key=\' + \$(\'[name=${value.replaceAll('\\.', '\\\\\\\\.')}]\').val()" }
 		
 		def separador = " + \'&\' +"
 		
-		lista += lista2
-		lista += lista3
+		params += ids
+		params += names
 		
-		def str = lista.join(separador)
+		def str = params.join(separador)
 		
-		out << g.remoteFunction(controller: attrs.controller, action: attrs.action, id: attrs.id, update: attrs.update ,params: str)
+		out << g.remoteFunction(controller: attrs.controller, action: attrs.action, id: attrs.id, update: attrs.update, params: str)
 	}
 }
